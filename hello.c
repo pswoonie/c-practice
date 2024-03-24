ca12134458278b79b94ca8e373fbce0ab75df786
pswoonie <164717677+pswoonie@users.noreply.github.com>
2024-03-24 21:19:06 +0900

    [UPDATE] show initial info (#2)
    * add spacing to readme heading
    
    * [UPDATE] print initial set

--- hello.c
+++ hello.c
@@ -1,20 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "oop.h"
 
-typedef struct{
-    string name;
-    int age;
-} Person;
-
-typedef struct{
-    string country;
-    Person person;
-} Human;
-
+#define BUFFERSIZE 100
 
 int main(int argc, char *argv[]) {
-    Human human = {"korea", {"andy", 30}}; 
-    printString(human.country, human.person.name, human.person.age);
+    string input = malloc(BUFFERSIZE);
+
+    Class *account1 = newChild("Andy");
+    Class *account2 = newChild("Jaquine");
+    Class *account3 = newChild("Hoa Hoa");
+    Class *account4 = newChild("Millanio");
+
+    fputs("|   ID   |   NAME   |   BALANCE   \n", stdout);
+    fputs("----------------------------------", stdout);
+    printf("|    %d   | %s     | %d   \n", account1->id, account1->name, account1->balance);
+    printf("|    %d   | %s  | %d   \n", account2->id, account2->name, account2->balance);
+    printf("|    %d   | %s  | %d   \n", account3->id, account3->name, account3->balance);
+    printf("|    %d   | %s | %d   \n", account4->id, account4->name, account4->balance);
+    fputs("\n--ENTER <exit> to TERMINATE \n\n",stdout);
+
+    while(strncmp(input, "exit", 4) != 0) {
+        fflush(stdin);
+        memset(input, 0, BUFFERSIZE);
+        fgets(input,sizeof(input),stdin);
+        fputs(input, stdout);
+    }
+
+    free(input);
+    free(account1);
+    free(account2);
+    free(account3);
+    free(account4);
+
+    input = NULL;
+    account1 = NULL;
+    account2 = NULL;
+    account3 = NULL;
+    account4 = NULL;
 
     return 0;
 }
